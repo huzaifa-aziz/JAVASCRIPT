ca137872d0193e2c229167dca8ace6818d9a18fe
Huzaifa Aziz <106053543+huzaifa-aziz@users.noreply.github.com>
2024-02-03 09:25:52 -0800

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,44 @@
+// String
+let name = "John";
+
+// Number
+let age = 25;
+
+// Boolean
+let isStudent = true;
+
+// Null
+let nullValue = null;
+
+// Undefined
+let undefinedValue;
+
+// Object
+let person = {
+  firstName: "Alice",
+  lastName: "Smith",
+  age: 30,
+};
+
+// Array
+let fruits = ["apple", "banana", "orange"];
+
+// Function
+function greet() {
+  console.log("Hello!");
+}
+
+// Symbol (introduced in ECMAScript 6)
+let uniqueKey = Symbol("unique");
+
+// Outputting the values
+console.log("String:", name);
+console.log("Number:", age);
+console.log("Boolean:", isStudent);
+console.log("Null:", nullValue);
+console.log("Undefined:", undefinedValue);
+console.log("Object:", person);
+console.log("Array:", fruits);
+console.log("Function:");
+greet();
+console.log("Symbol:", uniqueKey);
